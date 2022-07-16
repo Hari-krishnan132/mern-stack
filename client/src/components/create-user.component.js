e4f13b16a8a775aecec51ca02e3172838a097014
V Harikrishnan <68494920+Hari-krishnan132@users.noreply.github.com>
2022-07-17 01:48:02 +0530

    Update create-user.component.js

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -175,7 +175,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post('http://localhost:5000/users/add', user)
+    axios.post('/api/users/add', user)
       .then(res => {
         console.log(res);
         window.location = '/user/profile/'+res.data.id;
